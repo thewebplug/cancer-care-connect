eada0b675cc5ba11457808e8a9120af464280e0f
hammedTiamiyu <hammedtiamiyu18@gmail.com>
2023-09-14 11:47:04 +0100

    created endpoint mission and about page

--- server/db.js
+++ server/db.js
@@ -1,14 +1,14 @@
-import postgres from 'postgres'
-import dotenv from "dotenv";
-dotenv.config();
+// import postgres from 'postgres'
+// import dotenv from "dotenv";
+// dotenv.config();
 
-const sql = postgres(process.env.DATABASE_URL, { ssl: 'require' });
+// const sql = postgres(process.env.DATABASE_URL, { ssl: 'require' });
 
-async function getPostgresVersion() {
-    const result = await sql`select version()`;
-    console.log(result);
-  }
+// async function getPostgresVersion() {
+//     // const result = await sql`select version()`;
+//     console.log(result);
+//   }
 
-getPostgresVersion()
+// getPostgresVersion()
 
-export default sql
+// export default sql
---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+// const express = require("express");
+// import sql from "./db.js";
+import cors from "cors"
+
+const app = express();
+
+app.use(
+    cors({
+      origin: ["http://localhost:5173"],
+    })
+  );
+
+  app.get("/api/v1", (req, res) => {
+    res.send("Hello")
+})
+
+app.use(express.json())
+
+app.get("/api/v1/about", (req, res) => {
+    res.send('Welcome to cancer care connect (C C C). Never feel lonely, ccc is here to support you')
+})
+
+app.get("/api/v1/mission", (req, res) => {
+    res.send('Our mission is to build a platform to support people living with cancer ailment and to educate healthy individual on preventive measures')
+})
+
+  const PORT = 32000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
