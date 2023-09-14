2402ab35ed37a7f3d4635543199c7603d46bf692
Saleem Jibril <saleemjibril5@gmail.com>
2023-09-14 11:23:09 +0100

    added signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Vector from "../img/Vector.png";
-import Avatars from "../img/Colleagues discussing startup idea - 640x427 1.png";
+import Avatars from "../img/Web Developer doing coding - 640x533 1.png";
 import Img1 from "../img/img1.png";
 import Img2 from "../img/Cancer_Care_Connect 2.png";
 import Conncetion from "../img/undraw_connected_re_lmq2 1.png";
@@ -20,35 +20,45 @@ const Signup = () => {
           <div className="flex justify-between items-center mx-16">
             <div>
               <div className="text-[58px] mb-[33px] font-[700]">
-                Your Personalized Cancer Support Web Application
+              Join us today <br /> and get the <br /> attention you need
               </div>
               <div className="text-[20px] text-justify">
-                Cancer Care Connect is a cutting-edge web application designed
-                exclusively for cancer patients, survivors, caregivers, and their
-                families.
+              Are you or a loved one battling cancer and seeking <br /> a reliable companion throughout your journey? <br /> Look no further – Cancer Care Connect is here to <br /> empower, inform, and uplift you every step of the way.
               </div>
             </div>
             <img className="w-[728px] mt-[90px]" src={Avatars} alt="" />
           </div>
-  
-          <div className="grid grid-cols-2 gap-[94px] justify-start mt-[155px] mb-[168px] mx-16">
-            <img className="w-[731px]" src={Conncetion} alt="" />
-            <div className="">
-              <div className="text-[48px] font-[400]">Get the attention you deserve</div>
-              <div className="text-[20px] text-justify">
-              By offering a secure and user-friendly space, we aim to bridge the gap in emotional support and valuable insights for those facing the challenges of cancer
-  
-              <br />
-              <br />
-              "Cancer Care Connect" aims to provide a lifeline of support, information, and empowerment for those affected by cancer. By combining community, resources, and expert guidance, we aspire to make a positive impact on the lives of individuals navigating the challenges of cancer. Together, we can create a strong and compassionate network that fosters hope, resilience, and meaningful connections.
-  
-              </div>
-             
+          <div className="bgTwo">
+            <div className="w-[80%] m-auto text-[72px] font-[700] mb-[20px]">Sign Up</div>
+            <div className="w-[80%] m-auto text-[36px] font-[700] mb-[40px] ">Please Sign Up To Continue</div>
+            <div className=" w-[80%] grid grid-cols-2 gap-[47px] m-auto">
+
+            <input className="bg-[#fff] border border-[#9e9e9e] p-[5px] rounded-[8px]" type="text" placeholder="First Name" />
+                <input className="bg-[#fff] border border-[#9e9e9e] p-[5px] rounded-[8px]" type="text" placeholder="Last Name" />
+                <input className="bg-[#fff] border border-[#9e9e9e] p-[5px] rounded-[8px]" type="text" placeholder="Mobile Number" />
+                <input className="bg-[#fff] border border-[#9e9e9e] p-[5px] rounded-[8px]" type="text" placeholder="Email" />
+                <input className="bg-[#fff] border border-[#9e9e9e] p-[5px] rounded-[8px]" type="date" placeholder="Email" />
+                <select className="bg-[#fff] border border-[#9e9e9e] p-[5px] rounded-[8px]" type="text" placeholder="Email" >
+                    <option value="">Gender</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                </select>
+                <input className="bg-[#fff] border border-[#9e9e9e] p-[5px] rounded-[8px]" type="password" placeholder="Password" />
+                <input className="bg-[#fff] border border-[#9e9e9e] p-[5px] rounded-[8px]" type="password" placeholder="Confirm Password" />
+
+                
             </div>
-          </div>
-          {/* <ContactForm /> */}
-          
-          {/* <img  src={Right} alt="" /> */}
+            {/* <div className="w-full flex justify-center mt-[47px] h-[167px]">
+                <textarea className="bg-[#fff] w-[80%] border border-[#9e9e9e] p-[5px] rounded-[8px]" placeholder="Message" name="" id=""></textarea>
+                </div> */}
+
+               <div className="w-full flex justify-center">
+               <button className="bg-[#5AB9EB] w-[400px] h-[45px] m-auto mt-[38px] rounded-[15px] text-[#fff] shadow-[2px_5px_4px_0px_rgba(199,199,199,0.25)]">
+              Register
+            </button>
+               </div>
+        </div>
+        
   
         </div>
         <Footer />
