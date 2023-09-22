cd18fb52d02466f0efa721f04249dbe1d82ecd51
Saleem Jibril <saleemjibril5@gmail.com>
2023-09-22 12:03:50 +0100

    created Jouornal page

---
+++ client/src/pages/Journal.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const Journal = () => {
+  return (
+    <>
+      <Header />
+      <div>Journal</div>
+      <Footer />
+    </>
+  );
+};
+
+export default Journal;
