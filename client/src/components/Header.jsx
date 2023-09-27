122ae58fc80383a1aac91ed2d10904a9e93f21f4
Thomasosho <tomasosho@gmail.com>
2023-09-27 08:11:22 +0100

    Header Styling correctiong

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,32 +10,38 @@ import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const Header = () => {
   const location = useLocation();
+  const { auth } = useSelector((state) => ({ ...state }));
 
   const navigation = [
     {
       name: "Home",
       href: "/",
       current: location.pathname === "/" ? true : false,
+      hide: !auth?.token ? false : true,
     },
     {
       name: "About",
       href: "/about",
       current: location.pathname === "/about" ? true : false,
+      hide: !auth?.token ? false : true,
     },
     {
       name: "Contact",
       href: "/contact",
       current: location.pathname === "/contact" ? true : false,
+      hide: !auth?.token ? false : true,
     },
     {
       name: "Forum",
       href: "/forum",
       current: location.pathname === "/forum" ? true : false,
+      hide: !auth?.token ? true : false,
     },
     {
       name: "Journal",
       href: "/journal",
       current: location.pathname === "/journal" ? true : false,
+      hide: !auth?.token ? true : false,
     },
   ];
 
@@ -43,7 +49,6 @@ const Header = () => {
     return classes.filter(Boolean).join(" ");
   }
 
-  const { auth } = useSelector((state) => ({ ...state }));
   const navigate = useNavigate();
 
   const dispatch = useDispatch();
@@ -108,12 +113,9 @@ const Header = () => {
                         width="38"
                         height="43.0116"
                         filterUnits="userSpaceOnUse"
-                        color-interpolation-filters="sRGB"
+                        colorInterpolationFilters="sRGB"
                       >
-                        <feFlood
-                          flood-opacity="0"
-                          result="BackgroundImageFix"
-                        />
+                        <feFlood floodOpacity="0" result="BackgroundImageFix" />
                         <feColorMatrix
                           in="SourceAlpha"
                           type="matrix"
@@ -147,7 +149,7 @@ const Header = () => {
                         y2="17.5058"
                         gradientUnits="userSpaceOnUse"
                       >
-                        <stop stop-color="#FFC0C0" />
+                        <stop stopColor="#FFC0C0" />
                       </linearGradient>
                       <linearGradient
                         id="paint1_linear_1_3641"
@@ -157,7 +159,7 @@ const Header = () => {
                         y2="18"
                         gradientUnits="userSpaceOnUse"
                       >
-                        <stop stop-color="#FFC0C0" />
+                        <stop stopColor="#FFC0C0" />
                       </linearGradient>
                       <linearGradient
                         id="paint2_linear_1_3641"
@@ -167,14 +169,13 @@ const Header = () => {
                         y2="18"
                         gradientUnits="userSpaceOnUse"
                       >
-                        <stop stop-color="#FFC0C0" />
+                        <stop stopColor="#FFC0C0" />
                       </linearGradient>
                     </defs>
                   </svg>
                   <div className="font-bold">Cancer Care Connect</div>
                 </div>
                 <div className="hidden md:ml-6 md:block">
-               
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <Link
@@ -184,15 +185,14 @@ const Header = () => {
                           item.current
                             ? "bg-[#efacac] text-white"
                             : "text-dark hover:bg-[#efacac] hover:text-white",
-                          "rounded-md px-3 py-2 text-sm font-medium"
+                          "rounded-md px-3 py-2 text-sm font-medium",
+                          item.hide ? " hidden" : " block"
                         )}
                         aria-current={item.current ? "page" : undefined}
                       >
                         {item.name}
                       </Link>
                     ))}
-
-                   
                   </div>
                 </div>
               </div>
@@ -209,23 +209,21 @@ const Header = () => {
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
-                      stroke-width="1.5"
+                      strokeWidth="1.5"
                       stroke="currentColor"
-                      class="w-6 h-6"
+                      className="w-6 h-6"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
                       />
                     </svg>
 
-                    
-
                     {/* <MessageIcon className="h-6 w-6" aria-hidden="true" /> */}
                   </button>
                 )}
-                
+
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
@@ -238,13 +236,13 @@ const Header = () => {
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
                           viewBox="0 0 24 24"
-                          stroke-width="1.5"
+                          strokeWidth="1.5"
                           stroke="#ffffff"
-                          class="w-6 h-6"
+                          className="w-6 h-6"
                         >
                           <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
                           />
                         </svg>
@@ -252,36 +250,30 @@ const Header = () => {
                     )}
                     {!auth?.token && (
                       <div className="flex justify-center items-center">
+                        <Link to="/signin">
+                          <button
+                            type="button"
+                            className="mr-5 hidden md:block p-1 text-dark hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                          >
+                            <span className="absolute -inset-1.5" />
+                            <span className="sr-only">View notifications</span>
+                            <div>Login</div>
 
+                            {/* <MessageIcon className="h-6 w-6" aria-hidden="true" /> */}
+                          </button>
+                        </Link>
 
-                  <Link to="/signin">
-                  <button
-                    type="button"
-                    className="mr-5 hidden md:block p-1 text-dark hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  >
-                    <span className="absolute -inset-1.5" />
-                    <span className="sr-only">View notifications</span>
-                    <div>Login</div>
+                        <Link to="/signup">
+                          <button
+                            type="button"
+                            className="hidden md:block bg-[#5AB9EB] hover:bg-[#45a2d4] px-5 h-[49px] relative rounded-[10px] p-1 text-white  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                          >
+                            <div>Sign Up</div>
 
-                    {/* <MessageIcon className="h-6 w-6" aria-hidden="true" /> */}
-                  </button>
-                  </Link>
-                  
-             
-
-<Link to="/signup">
-                        <button
-                        type="button"
-                        className="hidden md:block bg-[#5AB9EB] hover:bg-[#45a2d4] px-5 h-[49px] relative rounded-[10px] p-1 text-white  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      >
-                        <div>Sign Up</div>
-
-                        {/* <MessageIcon className="h-6 w-6" aria-hidden="true" /> */}
-                      </button>
-                      </Link>
-
+                            {/* <MessageIcon className="h-6 w-6" aria-hidden="true" /> */}
+                          </button>
+                        </Link>
                       </div>
-                     
                     )}
                   </div>
                   <Transition
