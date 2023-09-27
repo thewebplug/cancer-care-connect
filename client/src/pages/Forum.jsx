7125f9ca63ef89e9fc77b33eb808aac01e7d08a6
Thomasosho <tomasosho@gmail.com>
2023-09-27 14:13:32 +0100

    Updated Forum List Topics

--- client/src/pages/Forum.jsx
+++ client/src/pages/Forum.jsx
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Happy from "../img/happy.png";
+import { useEffect, useState } from "react";
+import { Urls } from "../../src/routes/urls";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 const Forum = () => {
   const [firstname, setFirstName] = useState("");
@@ -10,6 +14,8 @@ const Forum = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
+  const { auth } = useSelector((state) => ({ ...state }));
+  const [forum, setForum] = useState([]);
 
   const handleSendMessage = async (e) => {
     setLoading(true);
@@ -46,6 +52,28 @@ const Forum = () => {
     }
     setLoading(false);
   };
+
+  useEffect(() => {
+    const fetchForum = async () => {
+      try {
+        const response = await fetch(
+          `${Urls?.baseUrl}${Urls?.getForum}/${auth.userInfo.id}`
+        );
+
+        if (!response.ok) {
+          alert("Network response was not ok");
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        setForum(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchForum();
+  }, []);
+
   return (
     <>
       <Header />
@@ -65,42 +93,44 @@ const Forum = () => {
         </div>
 
         <div className="w-full mb-10">
-          <Link className=" my-4 text-white rounded-tl-lg rounded-br-[400px] rounded-tr-[200px] border border-red-100 hover:border-red-400 p-4 bg-gradient-to-r from-blue-200 to-pink-300 hover:bg-gradient-to-r hover:from-blue-400 hover:to-pink-600">
+          <Link to={`/myforum/${auth.userInfo.id}`} className=" my-4 text-white rounded-tl-lg rounded-br-[400px] rounded-tr-[200px] border border-red-100 hover:border-red-400 p-4 bg-gradient-to-r from-blue-200 to-pink-300 hover:bg-gradient-to-r hover:from-blue-400 hover:to-pink-600">
             + Create New Topic
           </Link>
         </div>
 
         {/* Posts */}
-        <div className="w-full max-w-sm bg-white hover:border hover:border-gray-200 rounded-lg hover:shadow my-5">
-          <a href="#">
-            <img
-              className="p-8 rounded-t-lg"
-              src={Happy}
-              alt="Happy User"
-            />
-          </a>
-          <div className="px-5 pb-5">
+        {forum.map((forum, i) => (
+          <div className="w-full max-w-sm bg-white hover:border hover:border-gray-200 rounded-lg hover:shadow my-5">
             <a href="#">
-              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-              </h5>
+              <img
+                className="p-8 rounded-t-lg"
+                src={Happy}
+                alt="Happy User"
+              />
             </a>
-            <div className="flex items-center mt-2.5 mb-5">
-             
-            </div>
-            <div className="flex items-center justify-between">
-              <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                $599
-              </span>
-              <a
-                href="#"
-                className="text-white bg-gradient-to-r from-blue-200 to-pink-300 hover:bg-gradient-to-r hover:from-blue-400 hover:to-pink-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                View
+            <div className="px-5 pb-5">
+              <a href="#">
+                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                  {forum.title}
+                </h5>
               </a>
+              <div className="flex items-center mt-0.2 mb-2">
+              
+              </div>
+              <div className="flex items-center justify-between">
+                <span className="text-sm font-normal text-gray-900 dark:text-white">
+                  {forum.description}
+                </span>
+                {/* <a
+                  href="#"
+                  className="text-white bg-gradient-to-r from-blue-200 to-pink-300 hover:bg-gradient-to-r hover:from-blue-400 hover:to-pink-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  View
+                </a> */}
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
       <Footer />
     </>
