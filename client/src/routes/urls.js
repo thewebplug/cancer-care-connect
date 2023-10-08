bb51f5a38ad08bcb3e3f377d8308ae79ffeab287
Saleem Jibril <saleemjibril5@gmail.com>
2023-10-08 12:34:31 +0100

    updated backend base url

--- client/src/routes/urls.js
+++ client/src/routes/urls.js
@@ -1,7 +1,8 @@
 export const Urls = {
     // nointernet: 'No internet Connection',
     // baseUrl: 'http://localhost:3100/api/v1',
-    baseUrl: 'https://cancer-care-connect-3urv-backend.vercel.app/',
+    baseUrl: 'https://cancercareconnect.onrender.com/api/v1/',
+    
    
     register: '/register',
     login: '/login',
